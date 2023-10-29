87ba7dfb96ff51e676e8b597b076ea560b61c895
bluemangoo <chenfy2006@qq.com>
2023-10-29 11:01:33 +0800

    upgrade dependencies

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ app.get('/', (_req, res) => {
   res.send('Hello Typescript world!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
